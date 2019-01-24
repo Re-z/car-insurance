d33a22fdc96a5cb3054bf93ab5679472745839f1
re-z <re-z@ukr.net>
2019-01-25 00:33:27 +0200

    oop started

--- js/app.js
+++ js/app.js
@@ -5,28 +5,52 @@ let form = document.getElementById('request-quote');
 document.addEventListener('DOMContentLoaded', function(){
 
     html.printLatest20Years();
-    form.addEventListener('submit', handleSubmit)
+    form.addEventListener('submit', insurance.handleSubmit)
 
 })
 //functions
 
 
-function handleSubmit(evt) {
-    //prevent default submiting
-    evt.preventDefault();
-    //get form values
-    let selectedMake = document.querySelector('#make').value,
-        selectedYear = document.querySelector('#year').value,
-        //get value of selected checkbox
-        selectedLevel = document.querySelector('.form-check-input[name="level"]:checked').value;
-    //check if all fields are selected
-    if(selectedMake && selectedYear && selectedLevel) {
-        alert(`make = ${selectedMake} and year = ${selectedYear} and checked level = ${selectedLevel}`);
-    } else {
-        alert('fooo')
-    }    
-}
 
+
+class Insurance {
+    handleSubmit(evt) {
+        //prevent default submiting
+        evt.preventDefault();
+        //get form values
+        let selectedMake = document.querySelector('#make').value,
+            selectedYear = document.querySelector('#year').value,
+            //get value of selected checkbox
+            selectedLevel = document.querySelector('.form-check-input[name="level"]:checked').value;
+        //check if all fields are selected
+        if(selectedMake && selectedYear && selectedLevel) {
+            let result,
+            basis = 2000;
+        //calculate price, based on selected make
+        switch(selectedMake) {
+            case '1':
+                result = basis * 1.3;
+                break
+            case '2':
+                result = basis * 1.05;
+                break;
+            case '3':
+                result = basis * 1.4;
+                break;
+        }
+        //calculate price, based on year
+        let yearDiff = new Date().getFullYear();
+        yearDiff = yearDiff - selectedMake;
+        alert(yearDiff - s)
+            // alert(typeof selectedMake)
+            // alert(`make = typeof ${selectedMake}, result = ${result}, basis = ${basis} and year = ${selectedYear} and checked level = ${selectedLevel}`);
+        } else {
+            alert('fooo')
+        }    
+    }
+    
+}
+let insurance = new Insurance;
 
 class HTMLUI {
     printLatest20Years() {
@@ -39,7 +63,6 @@ class HTMLUI {
             yearsNode.insertAdjacentHTML('beforeend', newOption);
         }
     }
-
 };
 
 let html = new HTMLUI;

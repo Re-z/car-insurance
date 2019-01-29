953ed2af9b99a2a3f5dc4c8956892fb331e70795
re-z <re-z@ukr.net>
2019-01-29 21:05:36 +0200

    started refactoring

--- js/app.js
+++ js/app.js
@@ -1,68 +1,58 @@
 //vars
 let form = document.getElementById('request-quote');
 
-//listeners
-document.addEventListener('DOMContentLoaded', function(){
-
-    html.printLatest20Years();
-    form.addEventListener('submit', insurance.handleSubmit)
-
-})
 //functions
-
-
-
-
-class Insurance {
-    handleSubmit(evt) {
-        //prevent default submiting
-        evt.preventDefault();
-        //get form values
-        let selectedMake = document.querySelector('#make').value,
-            selectedYear = document.querySelector('#year').value,
-            //get value of selected checkbox
-            selectedLevel = document.querySelector('.form-check-input[name="level"]:checked').value;
-        //check if all fields are selected
-        if(selectedMake && selectedYear && selectedLevel) {
-            let result,
-            basis = 2000;
-        //calculate price, based on selected make
-        switch(selectedMake) {
-            case '1':
-                result = basis * 1.3;
-                break
-            case '2':
-                result = basis * 1.05;
-                break;
-            case '3':
-                result = basis * 1.4;
-                break;
-        }
-        //calculate price, based on year
-        let yearDiff = new Date().getFullYear();
-        yearDiff = yearDiff - selectedMake;
-        alert(yearDiff - s)
-            // alert(typeof selectedMake)
-            // alert(`make = typeof ${selectedMake}, result = ${result}, basis = ${basis} and year = ${selectedYear} and checked level = ${selectedLevel}`);
-        } else {
-            alert('fooo')
-        }    
+function handleSubmit(event) {
+    //get form values
+    let selectedMake = document.querySelector('#make').value,
+        selectedYear = document.querySelector('#year').value,
+        //get value of selected checkbox
+        selectedLevel = document.querySelector('.form-check-input[name="level"]:checked').value;
+    //check if all fields are selected
+    if(selectedMake && selectedYear && selectedLevel) {
+        let result,
+        basis = 2000;
+    //calculate price, based on selected make
+    switch(selectedMake) {
+        case '1':
+            result = basis * 1.3;
+            break
+        case '2':
+            result = basis * 1.05;
+            break;
+        case '3':
+            result = basis * 1.4;
+            break;
     }
-    
-}
-let insurance = new Insurance;
-
-class HTMLUI {
-    printLatest20Years() {
-        let yearsNode = document.querySelector('#year'),
-            currYear = new Date().getFullYear(), //returns curr year
-            minYear = currYear - 20; //
-        //print 20 last years and insert it to select element in DOM 
-        for (let i = currYear; i > minYear; i--){
-            let newOption = `<option value="${i}">${i}</option>`;
-            yearsNode.insertAdjacentHTML('beforeend', newOption);
-        }
+    //calculate price, based on year
+    let yearDiff = new Date().getFullYear();
+    yearDiff = yearDiff - selectedMake;
+    alert(yearDiff - s);
+        // alert(typeof selectedMake)
+        // alert(`make = typeof ${selectedMake}, result = ${result}, basis = ${basis} and year = ${selectedYear} and checked level = ${selectedLevel}`);
+    } else {
+        alert('fooo')
+    }    
+};
+function printLatest20Years() {
+    let yearsNode = document.querySelector('#year'),
+        currYear = new Date().getFullYear(), //returns curr year
+        minYear = currYear - 20; //
+    //print 20 last years and insert it to select element in DOM 
+    for (let i = currYear; i > minYear; i--){
+        let newOption = `<option value="${i}">${i}</option>`;
+        yearsNode.insertAdjacentHTML('beforeend', newOption);
     }
 };
 
-let html = new HTMLUI;
+//listeners
+document.addEventListener('DOMContentLoaded', function(){
+
+    printLatest20Years();
+    form.addEventListener('submit', function(evt){
+        //prevent default submiting
+        evt.preventDefault();
+        handleSubmit();
+    });
+
+})
